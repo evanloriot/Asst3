9e2850321aad086ea44c273c040cc10d3e7149ad
harrisonfarrugia <35969181+harrisonfarrugia@users.noreply.github.com>
2018-04-27 14:05:45 -0400

    Update client.c

--- client.c
+++ client.c
@@ -10,16 +10,23 @@
 #include "libnetfiles.h"
 
 int main(){
-	netserverinit("design.cs.rutgers.edu", "unrestricted");
-	char * b = "asdlkjfhaslkjdfhlaksjdhflkjashdflkjashdlfkjhasdlkjfhaslkjdhflkjashdflkjashdlkjfhaslkjdfhaslkjdhflaksjdhflkjashdflkjashdflkjahsdlkfjhaslkdjfhasljkdhfljkashdfljkashdfjlkhasljkdfhlaskjdhflkjashdflkjashdlkfjhasldkjfhaslkjdhflkjashdflkjahsdlkfjhaslkdjfhasl.txt";
-	int fd = netopen("../test/test.c", O_RDWR);
+	int z = connectToServer("flyweight.cs.rutgers.edu");
+	printf("%d\n", z);
+	int i = netserverinit("flyweight.cs.rutgers.edu", "unrestricted");	
+	printf("%d\n", i);
+	char * b = "asdfshjshshshbfkjfjakjskskdhaksdhk";
+	int fd = netopen("../test/test.c", O_RDWR);	
+//	fd = netopen("../test/test.c", O_RDONLY);
+//	int x;
 	int w = netwrite(fd, b, strlen(b));
 	printf("Written: %d\n", w);
 	char buff[256];
+//	for(x = 0; x < 100; x++) {	
 	int r = netread(fd, buff, 12);
 	buff[12] = '\0';
-	printf("R: %d, Buff: %s\n", r, buff); 
-	int close = netclose(-1);
+	printf("R: %d, Buff: %s\n", r, buff);
+//	} 
+	int close = netclose(fd);
 	printf("Close: %d\n", close);
 	if(close < 0) perror("Error");
 	return 0;
